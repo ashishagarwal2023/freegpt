cf928aa782a0470d65a7c386a81d2cca25ffb5c8
Ark <aasheeshsaumya@gmail.com>
2024-03-11 23:54:12 +0530

    Init

---
+++ README.md
@@ -0,0 +1,81 @@
+# 🔥 FreeGPT
+
+[![Python](https://img.shields.io/badge/python-3.8-blue.svg)](https://img.shields.io/badge/python-3.8-blue.svg)
+
+_⭐️ Like this repo? please star & consider donating to keep it maintained_
+
+_💡 If OpenAI change their API, I will fix it as soon as possible, so <mark>Watch</mark> the repo if you want to be notified_
+
+_The purpose of this project is not to leak the API, its just mine practice. It's just for educational purposes._
+
+### Features
+
+- [x] Free Open API for everyone
+- [x] Change token
+- [x] Saves token and conversation
+- [x] Reset API's token and conversation when you want
+- [x] Open-source, deployed on Vercel, free for all
+- [x] No leak of any information (when sending prompt or resetting API's conversation)
+
+I would not add any illegal features to the API like token collection or conversation exporting, since it is not legal to view what someone might ask or their token. Here's what things, if you would contribute, to not add:
+
+- [ ] Token collection/saving
+- [ ] Saving prompts and/or responses to file
+- [ ] Logging any info
+
+Feel free to test our my project and star it. I reverse engineered ChatGPT API too, so the gpt.py in API directory is written by me. Thanks!
+
+## Usage
+
+Hosted on Vercel. The main route of URL: `https://flask-API-example-rouge.vercel.app/`. All API requests should be sent to this URL.
+
+### /
+
+It is not a API at the root, would redirect you back to the github project.
+
+### /api/token
+
+To change the API's token, send a POST request with a token specifying your token at /API/token:
+
+```bash
+curl -X POST -d "token=YOUR_ACCESS_TOKEN_HERE" http://localhost:3000/API/token
+```
+
+> Server will automatically test your token by sending a test prompt. Rate-limited tokens would give you a key `error`, success will give key `message` with readable messages.
+
+### /api/prompt
+
+After the API is ready with a token, you can ask it prompts. API route: /API/prompt. Include a `prompt` with the request..
+
+```bash
+curl -X POST -d "prompt=Hey GPT!" http://localhost:3000/API/prompt
+```
+
+When no token was initalized with, the return would be:
+
+```json
+{
+  "error": "Server-related error, the token could have been rate limited. If you didn't specify a token, please specify it."
+}
+```
+
+On a success request:
+
+```json
+{ "response": "The API response here" }
+```
+
+### /api/reset
+
+To reset bot's conversation, send a POST request with no values at /api/reset
+
+```bash
+curl -X POST -d http://localhost:3000/api/reset
+```
+
+Bot will unspecify the token and the conversation ID, forgiving all you asked it.
+
+### Credits
+
+- [OpenAI](https://openai.com/) for creating the ChatGPT API
+- [ashishagarwal2023](https://github.com/ashishagarwal2023) for the API and reverse-engineering
---
+++ api/gpt.py
@@ -0,0 +1,105 @@
+import requests
+import json
+from uuid import uuid4
+
+# Just to simplify
+class TokenRateLimitedError(Exception):
+    def __init__(self, message="The token is rate limited. Try again later.", smth=""):
+        self.message = message
+        print(smth)
+        super().__init__(self.message)
+
+# The Main Class (Bot)
+class Bot:
+    def __init__(self, access_token: str = None): # it is optional however error without it
+        self.session = requests.Session()
+        self.session.headers["user-agent"] = "node"
+        self.access_token = access_token
+        self.conversation_id = None
+    
+    def get_access_token(self):
+        return self.access_token
+
+    def token(self, access_token: str):
+        self.access_token = access_token
+        self.conversation_id = None
+    
+    def reset(self):
+        self.conversation_id = None
+    
+    def verify(self, token):
+        toke = self.access_token
+        self.token(token)
+        try:
+            self.prompt("Test message to verify token")
+        except Exception as e:
+            self.access_token = toke
+            return False
+        else:
+            return True
+
+    def prompt(self, prompt_text: str) -> str:
+        body = {
+            "action": "next",
+            "conversation_mode": {"kind": "primary_assistant"},
+            "force_paragen": False,
+            "force_rate_limit": False,
+            "history_and_training_disabled": True,
+            "messages": [{
+                "metadata": {},
+                "author": {"role": "user"},
+                "content": {"content_type": "text", "parts": [prompt_text]}
+            }],
+            "model": "text-davinci-002-render-sha",
+            "parent_message_id": str(uuid4()),
+            "timezone_offset_min": -330
+        }
+
+        if self.conversation_id is not None:
+            try:
+                body["conversation_id"] = self.conversation_id
+            except:
+                raise TokenRateLimitedError("The token is rate limited. Try again later. From body conv id", body)
+
+        response = self.session.post(
+            url="https://chat.openai.com/backend-api/conversation",
+            headers={
+                "accept": "text/event-stream",
+                "accept-language": "en-US",
+                "authorization": f"Bearer {self.access_token}",
+                "content-type": "application/json",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "Referer": "https://chat.openai.com",
+                "Referrer-Policy": "strict-origin-when-cross-origin"
+            },
+            data=json.dumps(body)
+        )
+
+        data = {}
+        for chunk in response.text.split("\n"):
+            if chunk.startswith("data: {\"message\":"):
+                try:
+                    data = json.loads(chunk[6:])
+                except json.JSONDecodeError: # Invalid JSON
+                    raise Exception("Couldn't parse assistant's answer into a valid JSON. Might be a bug.")
+
+        try:
+            if data.get("conversation_id", "") != None:
+                self.conversation_id = data["conversation_id"]
+        except:
+            raise TokenRateLimitedError("The token is rate limited. Try again later. From line 74", data)
+            
+        if data["message"]["status"] != "finished_successfully": # Recieved prompt was not complete
+            raise Exception("The bot's message was not finished successfully, might be a error.")
+
+        if data["message"]["content"]["content_type"] != "text": # Recieved output is not text
+            raise Exception("The response did not gave the bot's response as text, most likely due to a bug.")
+
+        message_parts = data["message"]["content"]["parts"]
+        return "".join(message_parts)
+
+# bot = Bot("token here")
+# print(bot.prompt("Hello"))
+# bot.reset()
